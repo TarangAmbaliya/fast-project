2ae5e2b968e30ed5e7b3b21ffc551a0b872905cf
Tarang Ambaliya <taranga@webelight.co.in>
2022-06-06 15:31:00 +0530

    Modded with postgresql

--- core/config.py
+++ core/config.py
@@ -8,8 +8,8 @@ class Config(BaseSettings):
     DEBUG: bool = True
     APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
-    WRITER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
-    READER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
+    WRITER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
+    READER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
     JWT_SECRET_KEY: str = "fastapi"
     JWT_ALGORITHM: str = "HS256"
     SENTRY_SDN: str = None
@@ -20,21 +20,21 @@ class Config(BaseSettings):
 
 
 class DevelopmentConfig(Config):
-    WRITER_DB_URL: str = f"mysql+aiomysql://root:fastapi@db:3306/fastapi"
-    READER_DB_URL: str = f"mysql+aiomysql://root:fastapi@db:3306/fastapi"
+    WRITER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
+    READER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
     REDIS_HOST: str = "redis"
     REDIS_PORT: int = 6379
 
 
 class LocalConfig(Config):
-    WRITER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
-    READER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
+    WRITER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
+    READER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
 
 
 class ProductionConfig(Config):
     DEBUG: str = False
-    WRITER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/prod"
-    READER_DB_URL: str = f"mysql+aiomysql://fastapi:fastapi@localhost:3306/prod"
+    WRITER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
+    READER_DB_URL: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/usr"
 
 
 def get_config():
